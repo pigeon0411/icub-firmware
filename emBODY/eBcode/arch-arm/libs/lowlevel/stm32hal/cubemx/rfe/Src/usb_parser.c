922aa1d188fdf74596314d8451452d4a59b67e4c
maggia80 <marco.maggiali@iit.it>
2018-10-01 17:57:16 +0200

    added a usb parser

---
+++ emBODY/eBcode/arch-arm/libs/lowlevel/stm32hal/cubemx/rfe/Src/usb_parser.c
@@ -0,0 +1,16 @@
+ /****************************************************
+ *  						     *
+ *                                                   *
+ *                                                   *
+ *  Written by Marco Maggiali                        *
+ *  <marco.maggiali@iit.it>                          *
+ ****************************************************/
+ 
+#include <stdio.h>
+#include <string.h>
+#include "stm32l4xx_hal.h"
+#include "tim.h"
+#include "spi.h"
+#include "usb_parser.h"
+
+
